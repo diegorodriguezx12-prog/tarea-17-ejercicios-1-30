c7a57c35b28e922ff6b653bf6f877cb627f86f37
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 07:59:47 -0600

    ejercicio 2 terminado

--- ejercicio 1/Program.cs
+++ ejercicio 1/Program.cs
@@ -1,1 +1,13 @@
 ﻿Console.Write("Ingrese el primer número:");
+int numero1 = int.Parse(Console.ReadLine());
+Console.Write("Ingrese el segundo número:");
+int numero2 = int.Parse(Console.ReadLine());
+
+int resultado = sumarNumeros(numero1, numero2);
+Console.Write("La suma de los 2 números es: " + resultado);
+
+int sumarNumeros(int numero1, int numero2)
+{
+    int resultado = numero1 + numero2;
+    return resultado;
+}
