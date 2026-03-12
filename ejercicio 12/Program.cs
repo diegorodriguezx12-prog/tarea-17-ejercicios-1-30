bd47fcba9e0b8c1954c4e71c1b47248b7baa79b4
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 09:32:34 -0600

    ejercicio 12 terminado

--- ejercicio 12/Program.cs
+++ ejercicio 12/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.WriteLine("Ingrese su edad: ");
+int edad = int.Parse(Console.ReadLine());
+
+string resultado = ResultadoEdad(edad);
+Console.WriteLine(resultado);
+string ResultadoEdad(int edad)
+{
+    string resultado = "La edad ingresada es: " +edad+ " años";
+    return resultado;
+}
