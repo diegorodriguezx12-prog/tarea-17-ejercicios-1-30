a9456020c0e0702a9527a519302aceb9adfe74d4
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 09:19:58 -0600

    ejercicio 11 terminado

--- ejercicio 11/Program.cs
+++ ejercicio 11/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Ingrese su peso en kg: ");
+double peso = double.Parse(Console.ReadLine());
+
+double pesoEnLibras = ConvertirKgALibras(peso);
+Console.WriteLine("Su peso en libras es: " + pesoEnLibras);
+
+double ConvertirKgALibras(double peso)
+{
+    double pesoEnLibras = peso * 2.2;
+    return pesoEnLibras;
+}
