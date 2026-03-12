394a600b3726017db1d7e0589768fddfda058e90
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 17:59:25 -0600

    ejercicio 13 terminado

--- ejercicio 13/Program.cs
+++ ejercicio 13/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.Write("¿Cuántos productos compraste? ");
+int cantidad = int.Parse(Console.ReadLine());
+
+int contador = 1;
+double totalSuma = 0;
+while (contador <= cantidad)
+{
+    Console.Write("Ingrese el precio del producto " + contador + ": ");
+    double precio = double.Parse(Console.ReadLine());
+    totalSuma = totalSuma + precio;
+    contador++;
+}
+Console.WriteLine("El total de su compra es: Q" + totalSuma);
