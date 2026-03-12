5dd87c21b49e1219a9c469fb72e0da4c6c1e0459
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 08:09:10 -0600

    Ejercicio 3 terminado

---
+++ ejercicio 3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ingrese el número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int resultado = dobleDelNumero(numero);
+Console.Write("El doble del número es: " + resultado);
+
+int dobleDelNumero(int numero)
+{
+    int resultado = numero * 2;
+    return resultado;
+}
