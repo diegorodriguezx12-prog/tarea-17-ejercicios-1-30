474618bac0cccfbe872894b98e1b91efbc7cf0a9
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 09:02:16 -0600

    ejercicio 9 terminado

--- ejercicio 9/Program.cs
+++ ejercicio 9/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Ingrese Cuanto gana por hora");
+double salarioHora = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese cuantas horas trabaja al día");
+double horasDia = double.Parse(Console.ReadLine());
+
+double salarioSemanal = CalcularSalarioSemanal(salarioHora, horasDia);
+Console.WriteLine("El salario semanal es: " + salarioSemanal);
+
+double CalcularSalarioSemanal(double salarioHora, double horasDia)
+{
+    double salarioSemanal = salarioHora * horasDia * 7;
+    return salarioSemanal;
+}
