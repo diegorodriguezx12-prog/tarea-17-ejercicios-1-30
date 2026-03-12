ffdfff8977835395a03c0fb8d34c576e310ad3a6
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 08:21:24 -0600

    ejercicio 5 iniciado

---
+++ ejercicio 5/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Ingrese la base del rectángulo");
+double baseRectangulo = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la altura del rectángulo");
+double alturaRectangulo = double.Parse(Console.ReadLine());
+
+double  
