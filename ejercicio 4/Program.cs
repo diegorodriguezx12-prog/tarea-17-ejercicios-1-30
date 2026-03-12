779048287a1b797171e46fdff2bef65a5649af4f
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 08:17:50 -0600

    ejercicio 4 terminado

--- ejercicio 4/Program.cs
+++ ejercicio 4/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Ingrese el radio del círculo:");
+﻿Console.Write("Ingrese el radio del círculo: ");
+double radio = double.Parse(Console.ReadLine());
+
+double area = AreaDeUnCirculo(radio);
+Console.Write("El área del círculo es: " + area);
+
+double AreaDeUnCirculo(double radio)
+{
+    double area = Math.PI * radio * radio;
+    return area;
+}
