231925ba2cf929f34a5a3736f427f6d5fe94a811
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 08:04:11 -0600

    ejercicio 2 terminado

--- ejercicio 2/Program.cs
+++ ejercicio 2/Program.cs
@@ -1,2 +1,11 @@
-﻿Console.WriteLine("Ingrese el número que desea elevar al cuadrado:");
+﻿Console.Write("Ingrese el número que desea elevar al cuadrado: ");
 int numero = int.Parse(Console.ReadLine());
+
+int resultado = elevarNumeroAlCuadrado(numero);
+Console.WriteLine("El resultado de elevar el número al cuadrado es: " + resultado);
+
+int elevarNumeroAlCuadrado(int numero)
+{
+    int resultado = numero * numero;
+    return resultado;
+}
