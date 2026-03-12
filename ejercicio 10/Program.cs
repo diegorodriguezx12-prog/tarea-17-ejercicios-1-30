d64fb9ba5ff1e399dc2597c9232d39e406ae5712
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 09:12:59 -0600

    ejercicio 10 terminado

--- ejercicio 10/Program.cs
+++ ejercicio 10/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Write("Ingrese el número que quiera elevar al cubo: ");
+int numero = int.Parse(Console.ReadLine());
+
+int resultado = elevarNumeroAlCubo(numero);
+Console.Write("El resultado de elevar el número al cubo es: " + resultado);
+
+int elevarNumeroAlCubo(int numero)
+{
+    int resultado = numero * numero * numero;
+    return resultado;
+}
