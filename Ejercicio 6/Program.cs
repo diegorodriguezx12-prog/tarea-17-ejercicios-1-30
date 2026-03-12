4e34b5cd971590b7c04a820c685d8aa4061676c4
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 08:36:24 -0600

    ejercicio 6 terminado

--- Ejercicio 6/Program.cs
+++ Ejercicio 6/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Ingrese la temperatura en grados Celsius:");
+﻿Console.Write("Ingrese la temperatura en grados Celsius:");
+double gradosCelsius = double.Parse(Console.ReadLine());
+
+double gradosFahrenheit = convercionGradosCelsius(gradosCelsius);
+Console.Write("La temperatura en grados Fahrenheit es: " + gradosFahrenheit);
+double convercionGradosCelsius(double gradosCelsius)
+{
+    double gradosFahrenheit = gradosCelsius * 1.8 + 32;
+    return gradosFahrenheit;
+}
