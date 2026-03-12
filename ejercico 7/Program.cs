6a0541b34220edef3145b65fce2bd09dee945210
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 08:45:33 -0600

    ejercicio 7 terminado

--- ejercico 7/Program.cs
+++ ejercico 7/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.Write("Ingrece el precio del producto: ");
+double precio = double.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el porcentaje de descuento: ");
+double descuento = double.Parse(Console.ReadLine());
+
+double precioConDescuento = CalcularPrecioConDescuento(precio, descuento);
+Console.Write("El precio con descuento es: " + precioConDescuento);
+double CalcularPrecioConDescuento(double precio, double descuento)
+{
+    double precioConDescuento = precio - (precio * (descuento / 100));
+    return precioConDescuento;
+}
