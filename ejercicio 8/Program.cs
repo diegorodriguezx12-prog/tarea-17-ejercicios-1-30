66fca07ea10a4cff0134672af35a53ece72e0755
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 08:57:16 -0600

    ejercicio 8 terminado

--- ejercicio 8/Program.cs
+++ ejercicio 8/Program.cs
@@ -1,1 +1,17 @@
 ﻿Console.Write("Ingrese el primer número: ");
+double n1 = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el segundo número: ");
+double n2 = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el tercer número: ");
+double n3 = int.Parse(Console.ReadLine());
+
+double resultado = calcularpromedio(n1, n2, n3);
+Console.Write("El promedio de los 3 números es: " + resultado);
+
+double calcularpromedio(double n1, double n2, double n3)
+{
+    double resultado = (n1 + n2 + n3) / 3;
+    return resultado;
+}
