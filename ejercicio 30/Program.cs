151e442b85d0c6f747b63921f9c9dae3a00bafdf
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-13 01:03:11 -0600

    ejercicio 30 terminado

--- ejercicio 30/Program.cs
+++ ejercicio 30/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿void GenerarReporteEstudiante()
+{
+    Console.Write("Ingrese el nombre del estudiante: ");
+    string nombre = Console.ReadLine();
+
+    Console.Write("Ingrese el nombre del curso: ");
+    string curso = Console.ReadLine();
+
+    Console.Write("Ingrese la nota 1: ");
+    double n1 = double.Parse(Console.ReadLine());
+
+    Console.Write("Ingrese la nota 2: ");
+    double n2 = double.Parse(Console.ReadLine());
+
+    Console.Write("Ingrese la nota 3: ");
+    double n3 = double.Parse(Console.ReadLine());
+
+    double promedio = (n1 + n2 + n3) / 3;
+
+    Console.WriteLine("       REPORTE DE CALIFICACIONES      ");
+    Console.WriteLine("Estudiante: " + nombre);
+    Console.WriteLine("Curso:      " + curso);
+    Console.WriteLine("Nota 1:     " + n1);
+    Console.WriteLine("Nota 2:     " + n2);
+    Console.WriteLine("Nota 3:     " + n3);
+    Console.WriteLine("PROMEDIO:   " + promedio.ToString("N2"));
+}
