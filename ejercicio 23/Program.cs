b37a4e4169e4a6512cbd371f288884fe8385e420
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-13 00:20:34 -0600

    ejercicio 23 terminado

--- ejercicio 23/Program.cs
+++ ejercicio 23/Program.cs
@@ -1,1 +1,12 @@
-﻿w
+﻿void SolicitarYSumar()
+{
+    Console.Write("Ingrese el primer número: ");
+    double n1 = double.Parse(Console.ReadLine());
+
+    Console.Write("Ingrese el segundo número: ");
+    double n2 = double.Parse(Console.ReadLine());
+
+    double suma = n1 + n2;
+
+    Console.WriteLine("La suma de ambos números es: " + suma);
+}
