16bb4029c41d9759419e93eba40df68eb66d0e1d
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 18:23:50 -0600

    ejercicio 15 terminado

--- ejercicio 15/Program.cs
+++ ejercicio 15/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.WriteLine("--- Calculadora de Área de Triángulos ---");
+
+Console.Write("Ingrese la base del triángulo: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("Ingrese la altura del triángulo: ");
+double h = double.Parse(Console.ReadLine());
+
+double resultado = CalcularAreaTriangulo(b, h);
+Console.WriteLine("El área del triángulo es: " + resultado);
+
+double CalcularAreaTriangulo(double baseTri, double altura)
+{
+    double area = (baseTri * altura) / 2;
+    return area;
+}
