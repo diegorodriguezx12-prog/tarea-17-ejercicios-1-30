d87cb2fbfc063159371e7ff29233ba40f675fce2
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 23:58:13 -0600

    ejercicio 20 terminado

--- ejercicio 20/Program.cs
+++ ejercicio 20/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.WriteLine("--- Cálculo de Salario ---");
+
+Console.Write("Ingrese la cantidad de días trabajados: ");
+int dias = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el pago por día: ");
+double pago = double.Parse(Console.ReadLine());
+
+double totalRecibir = CalcularSalario(dias, pago);
+
+Console.WriteLine("El salario total a pagar es: Q" + totalRecibir);
+
+double CalcularSalario(int diasTrabajados, double pagoDiario)
+{
+    double salarioTotal = diasTrabajados * pagoDiario;
+    return salarioTotal;
+}
