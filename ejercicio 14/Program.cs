c8e14990fa81f9e53660fe9b49ea55b22f7edfe5
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 18:14:54 -0600

    ejercicio 14 terminado

--- ejercicio 14/Program.cs
+++ ejercicio 14/Program.cs
@@ -1,1 +1,15 @@
 ﻿Console.WriteLine("Ingrese su distancia recorrida (km): ");
+double distancia = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese su tiempo (Horas): ");
+double horas = double.Parse(Console.ReadLine());
+
+double resultado = CalcularVelocidadPromedio(distancia, horas);
+
+Console.WriteLine("La velocidad promedio es: " + resultado + " km/h");
+
+Double CalcularVelocidadPromedio(double distancia, double horas)
+{
+    double velocidadPromedio = distancia / horas;
+    return velocidadPromedio;
+}
