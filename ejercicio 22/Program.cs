e3bb425f12e8788f37c6768e98beb83d0bc1b1d9
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-13 00:14:07 -0600

    ejercicio 22 terminado

--- ejercicio 22/Program.cs
+++ ejercicio 22/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿void SolicitarNombreYSaludar()
+{
+    Console.Write("¿Cómo te llamas? ");
+    string nombre = Console.ReadLine();
+    Console.WriteLine("¡Mucho gusto, " + nombre + "! Es un placer ayudarte con tu código.");
+}
