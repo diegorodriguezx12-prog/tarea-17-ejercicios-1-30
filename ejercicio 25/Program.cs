23ca4d80b793c09f13bb49f801e8b1184acbaf48
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-13 00:33:35 -0600

    ejercicio 25 terminado

--- ejercicio 25/Program.cs
+++ ejercicio 25/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿void CalcularYMostrarAreaRectangulo()
+{
+    Console.Write("Ingrese la base del rectángulo: ");
+    double baseRect = double.Parse(Console.ReadLine());
+
+    Console.Write("Ingrese la altura del rectángulo: ");
+    double alturaRect = double.Parse(Console.ReadLine());
+
+    double area = baseRect * alturaRect;
+
+    Console.WriteLine("El área del rectángulo es: " + area);
+}
