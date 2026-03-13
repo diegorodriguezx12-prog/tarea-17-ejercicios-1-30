d35b567dbe5e60d2247b6b6445f5439be82f8d66
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-13 00:45:19 -0600

    ejercicio 27 terminado

--- ejercicio 27/Program.cs
+++ ejercicio 27/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿void CalcularDescuentoDiezPor Ciento()
+{
+    Console.Write("Ingrese el precio original del producto: Q");
+double precioOriginal = double.Parse(Console.ReadLine());
+
+double descuento = precioOriginal * 0.10;
+
+double precioFinal = precioOriginal - descuento;
+
+Console.WriteLine("Descuento aplicado (10%): Q" + descuento);
+Console.WriteLine("El precio final con descuento es: Q" + precioFinal);
+}
