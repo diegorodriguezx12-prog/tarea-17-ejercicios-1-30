3b8130811f55a5b726c121209224a6d3a67f39bd
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-13 00:26:59 -0600

    ejercicio 24 terminado

--- ejercicio 24/Program.cs
+++ ejercicio 24/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿void CalcularYMostrarAreaCirculo()
+{
+    Console.Write("Ingrese el radio del círculo: ");
+    double radio = double.Parse(Console.ReadLine());
+
+    double area = Math.PI * Math.Pow(radio, 2);
+
+    Console.WriteLine("El área del círculo con radio " + radio + " es: " + area);
+}
