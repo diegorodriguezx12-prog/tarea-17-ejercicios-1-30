26c8f8f0b41a29dff321d5d6245a8604a54302bb
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 18:46:09 -0600

    ejercicio 16 terminado

--- ejercicio 16/Program.cs
+++ ejercicio 16/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Write("Ingrese la cantidad de minutos: ");
+int minIngresados = int.Parse(Console.ReadLine());
+
+double resultadoHoras = ConvertirMinutosAHoras(minIngresados);
+
+Console.WriteLine(minIngresados + " minutos equivalen a: " + resultadoHoras + " horas.");
+
+double ConvertirMinutosAHoras(int minutos)
+{
+    double horas = minutos / 60.0;
+    return horas;
+}
