2afc6078e3e4405fc846bfb86b87d17258e2496b
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-13 00:08:36 -0600

    ejercicio 21 terminado

--- ejercicio 21/Program.cs
+++ ejercicio 21/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.Write("Por favor, ingresa tu nombre: ");
+string nombre = Console.ReadLine();
+
+MostrarBienvenida(nombre);
+
+void MostrarBienvenida(string nombreUsuario)
+{
+    Console.WriteLine("¡Bienvenido al sistema, " + nombreUsuario + "!");
+    Console.WriteLine("Es un gusto saludarte el día de hoy.");
+}
