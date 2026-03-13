a7d130a1b3e0a098cd4b3adc8d8812cb657bae4c
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-12 23:33:32 -0600

    ejercicio 18 terminado

--- ejercicio 18/Program.cs
+++ ejercicio 18/Program.cs
@@ -1,1 +1,29 @@
 ﻿Console.Write("¿Cuántos productos desea facturar? ");
+int cantidad = int.Parse(Console.ReadLine());
+
+double[] factura = new double[cantidad];
+int contador = 0;
+
+while (contador < cantidad)
+{
+    Console.Write("Ingrese el precio del producto " + (contador + 1) + ": ");
+    factura[contador] = double.Parse(Console.ReadLine());
+    contador++;
+}
+double totalFinal = CalcularTotalFactura(factura);
+
+Console.WriteLine("El total de su factura es: Q" + totalFinal);
+
+double CalcularTotalFactura(double[] precios)
+{
+    double total = 0;
+    int i = 0;
+
+    while (i < precios.Length)
+    {
+        total = total + precios[i];
+        i++;
+    }
+
+    return total;
+}
