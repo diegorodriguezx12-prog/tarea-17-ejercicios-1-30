a352a957ccd9e05a79b057af2581806bb213fce9
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-13 00:54:05 -0600

    ejercicio 29 terminado

--- ejercicio 29/Program.cs
+++ ejercicio 29/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿void MostrarCantidadEstudiantes()
+{
+    Console.Write("¿Cuántos estudiantes hay inscritos en el curso? ");
+    int cantidad = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Actualmente hay " + cantidad + " estudiantes en la clase.");
+}
