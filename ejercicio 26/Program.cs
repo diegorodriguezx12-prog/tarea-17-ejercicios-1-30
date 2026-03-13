24bb86ddf4c841333aea35fd999956a21684b3b7
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-13 00:39:04 -0600

    ejercicio 26 terminado

--- ejercicio 26/Program.cs
+++ ejercicio 26/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿void CalcularAnioNacimiento()
+{
+    Console.Write("Ingrese su edad: ");
+    int edad = int.Parse(Console.ReadLine());
+
+    int anioActual = 2026;
+    int anioNacimiento = anioActual - edad;
+
+    Console.WriteLine("Tu año aproximado de nacimiento es: " + anioNacimiento);
+    Console.WriteLine("(Nota: Esto puede variar dependiendo de si ya cumpliste años este año).");
+}
